946f60842db2b61c01cff029cb744588e43eb6c2
tjeerd <tjeerdintveen@ing.com>
2020-03-28 08:33:43 +0100

    Updated chapter 8

--- ch08-protocols/Putting the pro in protocol-oriented programming.playground/Pages/Table of contents.xcplaygroundpage/Contents.swift
+++ ch08-protocols/Putting the pro in protocol-oriented programming.playground/Pages/Table of contents.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 /*:
  
- # Effortless error handling
+ # Putting the pro in protocol-oriented programming
  
  Code listings and exercises for [Swift in Depth][Swift in Depth].
  
@@ -12,12 +12,12 @@
  
  Press command + 1 to see all contents.
  
- Go to Editor > Show Rendered Markup or Editor > Show Raw Markup, to switch between presentation styles.
+ Xcode: Go to Editor >  Show Raw Markup, to switch between presentation styles.
  
  [Tjeerd in 't Veen]: https://twitter.com/tjeerdintveen
  [Swift in Depth]: https://www.manning.com/books/swift-in-depth
  
- **Written in Swift 4.1 using Xcode 9.3.1**
+ **Written in Swift 5.0 using Xcode 11.3.1***
  
  ## Table of Contents
  
